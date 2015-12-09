bf1258c08ebbe7b255d8a44b1c119555d84a895d
Mico Piira <mico.piira@gmail.com>
2015-12-09 12:35:08 +0200

    Reduce build size
    Removed unneeded files from
    the package (such as: cache,
    LICENSE, README, CHANGELOG,
    gulpfile.js)

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var argv = require('yargs')
 var del = require('del');
 var detectCurrentPlatform = require('nw-builder/lib/detectCurrentPlatform.js');
 var nw = new NwBuilder({
-    files: './**',
+    files: ['./src/**', './node_modules/**', './package.json'],
     version: '0.12.3',
     platforms: argv.p ? argv.p.split(',') : [detectCurrentPlatform()]
 }).on('log', console.log);
