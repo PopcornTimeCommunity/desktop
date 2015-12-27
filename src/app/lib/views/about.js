56373a963eea8d50b869f45937ba7b2a47ca17a0
YIFY torrents <mehmet@tutanota.de>
2015-12-27 20:39:13 +0000

    Update about.js

--- src/app/lib/views/about.js
+++ src/app/lib/views/about.js
@@ -1,67 +1,72 @@
-(function (App) {
-    'use strict';
+<%
+    if (typeof image === 'undefined') { var image = images.poster; }
+    if (typeof rating === 'object') { var rating = rating.percentage /10; }
+%>
 
-    var About = Backbone.Marionette.ItemView.extend({
-        template: '#about-tpl',
-        className: 'about',
+<img class="cover-image" src="images/posterholder.png">
+<div class="cover">
+    <div class="cover-overlay">
+        <i class="fa fa-heart actions-favorites tooltipped" data-toggle="tooltip" data-placement="auto bottom" data-delay='{ "show": "800", "hide": "100" }'></i>
+        <i class="fa fa-eye actions-watched tooltipped" data-toggle="tooltip" data-placement="auto bottom" data-delay='{ "show": "800", "hide": "100" }'></i>
 
-        ui: {
-            success_alert: '.success_alert'
-        },
+        <% if(typeof rating !== 'undefined'){
+        var p_rating = Math.round(rating) / 2;  %>
+            <div class="rating" <% if(Settings.coversShowRating){ %> style="display: block;"<% } %> >
+                <div class="rating-stars">
+                    <% for (var i = 1; i <= Math.floor(p_rating); i++) { %>
+                        <i class="fa fa-star rating-star"></i>
+                    <% }; %>
+                    <% if (p_rating % 1 > 0) { %>
+                        <span class = "fa-stack rating-star-half-container">
+                            <i class="fa fa-star fa-stack-1x rating-star-half-empty"></i>
+                            <i class="fa fa-star-half fa-stack-1x rating-star-half"></i>
+                        </span>
+                    <% }; %>
+                    <% for (var i = Math.ceil(p_rating); i < 5; i++) { %>
+                        <i class="fa fa-star rating-star-empty"></i>
+                    <% }; %>
+                </div>
+                <div class="rating-value"><%= rating %>/10</div>
+            </div>
+        <%} %>
+    </div>
+</div>
 
-        events: {
-            'click .close-icon': 'closeAbout',
-            'click #changelog': 'showChangelog',
-            'click .title-issue': 'reportIssue'
-        },
+<p class="title" title="<%= title %>"><%= title %></p>
+<p class="year">
 
-        onShow: function () {
-            $('.filter-bar').hide();
-            $('#header').addClass('header-shadow');
+	<% if (typeof item_data !== 'undefined' || typeof num_seasons !== 'undefined') {%>
+	<%}else if (typeof torrents !== 'undefined') { %>
+		<% if (google_video !== false) {%>
+        	<img class="seasons quality" <% if(Settings.moviesShowQuality){ %> style="display:block; float:left; margin-right:3px;" <% } %> src="images/icons/googlecloud-favicon.png" width="15" title="Google Cloud" alt="Google Cloud" />
+    	<%} %>
+	<%} %>	
 
-            Mousetrap.bind(['esc', 'backspace'], function (e) {
-                App.vent.trigger('about:close');
-            });
-            $('.links,#changelog').tooltip();
-            win.info('Show about');
-            $('#movie-detail').hide();
-        },
+    <% if (typeof year !== 'undefined') {%>
+        <%= year %>
+    <%} %>
+</p>
 
-        onDestroy: function () {
-            Mousetrap.unbind(['esc', 'backspace']);
-            $('.filter-bar').show();
-            $('#header').removeClass('header-shadow');
-            $('#movie-detail').show();
-        },
 
-        closeAbout: function () {
-            if ($('.changelog-overlay').css('display') === 'block') {
-                this.closeChangelog();
-            } else {
-                App.vent.trigger('about:close');
-            }
-        },
-
-        showChangelog: function () {
-            fs.readFile('./CHANGELOG.md', 'utf-8', function (err, contents) {
-                if (!err) {
-                    $('.changelog-text').html(contents.replace(/\n/g, '<br />'));
-                    $('.changelog-overlay').show();
-                } else {
-                    gui.Shell.openExternal('https://github.com/PopcornTimeCE/popcorntimece/blob/master/CHANGELOG.md');
-                }
-            });
-        },
-
-        reportIssue: function () {
-            App.vent.trigger('issue:new');
-        },
-
-        closeChangelog: function () {
-            $('.changelog-overlay').hide();
-        }
-
-    });
-
-    App.View.About = About;
-})(window.App);
+<% if (typeof item_data !== 'undefined') {%>
+        <p class="seasons data">
+            <%= i18n.__(item_data) %>
+        </p>
+<% } else if(typeof num_seasons !== 'undefined'){%>
+    <p class="seasons">
+        <%= num_seasons %> <%= num_seasons == 1 ? i18n.__("Season") : i18n.__("Seasons") %>
+    </p>
+<%}else if (typeof torrents !== 'undefined') { %>
+    <p id="movie_quality" class="seasons quality" <% if(Settings.moviesShowQuality){ %> style="display: block;" <% } %> >
+        <% q720 = torrents["720p"] !== undefined; q1080 = torrents["1080p"] !== undefined;
+        if (q720 && q1080) { %>
+            720p/1080p
+        <% } else if (q1080) { %>
+            1080p
+        <% } else if (q720) { %>
+            720p
+        <% } else { %>
+            HDRip
+        <% } %>
+    </p>
+<%} %>
